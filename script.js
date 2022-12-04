2c39712472e3071bb20bd39ecc16e2303641c24f
AngerOverApathy <violasian@gmail.com>
2022-12-04 13:23:04 -0800

    created new note object

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
 class App {
-   constructor() {
-    this.$form = document.querySelector('#form') //$ added to recognize we are working with an html element
-    this.$noteTitle = document.querySelector('#note-title')
-    this.$formButtons = document.querySelector('#form-buttons')
-
-    this.addEventListeners();
-   } 
-
-   addEventListeners() {   //add method to app class
-      document.body.addEventListener('click', event => {
+    constructor() {
+      this.notes = []; //hold notes
+  
+      this.$form = document.querySelector("#form"); //$ added to recognize we are working with an html element
+      this.$noteTitle = document.querySelector("#note-title");
+      this.$noteText = document.querySelector("#note-text");
+      this.$formButtons = document.querySelector("#form-buttons");
+  
+      this.addEventListeners();
+    }
+  
+    addEventListeners() {  //add method to app class
+      document.body.addEventListener("click", event => {
         this.handleFormClick(event); //when using a class, use "this" to reference every method being called 
       });
-   }
-
-   handleFormClick(event){
-    const isFormClicked = this.$form.contains(event.target) //contains will return a boolean
-
-    if (isFormClicked) {
-        this.openForm()
-    } else {
-        this.closeForm()
+  
+      this.$form.addEventListener("submit", event => {
+        event.preventDefault(); //prevent form refresh
+        const title = this.$noteTitle.value;
+        const text = this.$noteText.value;
+        const hasNote = title || text; //need title or text for form to submit
+        if (hasNote) {
+          // add note
+          this.addNote({ title, text }); //object shorthand {title: title, text: text}
+        }
+      });
     }
-   }
-
-   openForm() {      //method on our class
-    this.$form.classList.add('form-open')
-    this.$noteTitle.style.display = 'block'
-    this.$formButtons.style.display = 'block'
-
-   }
-
-   closeForm() {
-    this.$form.classList.remove('form-open')
-    this.$noteTitle.style.display = 'none'
-    this.$formButtons.style.display = 'none'
-   }
-}
-
-new App()
+  
+    handleFormClick(event) {
+      const isFormClicked = this.$form.contains(event.target);  //contains will return a boolean
+  
+      if (isFormClicked) {
+        this.openForm();
+      } else {
+        this.closeForm();
+      }
+    }
+  
+    openForm() { //method on our class
+      this.$form.classList.add("form-open");
+      this.$noteTitle.style.display = "block";
+      this.$formButtons.style.display = "block";
+    }
+  
+    closeForm() {
+      this.$form.classList.remove("form-open");
+      this.$noteTitle.style.display = "none";
+      this.$formButtons.style.display = "none";
+    }
+  
+    addNote(note) {
+      const newNote = {
+        title: note.title,
+        text: note.text,
+        color: "white",
+        id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1 //if the length of notes array is > 0, add +1 to length of array, get id property, add 1 to increment id property
+      };
+      this.notes = [...this.notes, newNote]; //spread operator to make immutable array
+      console.log(this.notes);
+    }
+  }
+  
+  new App();
+  
