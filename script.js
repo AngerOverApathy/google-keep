89b0de968c4acd8ac970f4ab8f7887c677fc3f48
AngerOverApathy <violasian@gmail.com>
2022-12-07 08:00:50 -0800

    notes saved to page

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ class App {
     constructor() {
       this.notes = []; //hold notes
   
+      this.$placeholder = document.querySelector('#placeholder')
       this.$form = document.querySelector("#form"); //$ added to recognize we are working with an html element
+      this.$notes = document.querySelector('#notes');
       this.$noteTitle = document.querySelector("#note-title");
       this.$noteText = document.querySelector("#note-text");
       this.$formButtons = document.querySelector("#form-buttons");
@@ -57,7 +59,27 @@ class App {
         id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1 //if the length of notes array is > 0, add +1 to length of array, get id property, add 1 to increment id property
       };
       this.notes = [...this.notes, newNote]; //spread operator to make immutable array
-      console.log(this.notes);
+      this.displayNotes() //display notes
+    }
+
+    displayNotes() {
+        //hide placeholder if notes exist
+        const hasNotes = this.notes.length > 0;
+        this.$placeholder.style.display = hasNotes ? 'none' : 'flex' //if notes display notes, else display placeholder
+    
+        //display content by iterating over notes display
+        this.$notes.innerHTML = this.notes.map(note => `     
+        <div style="background: ${note.color};" class="note">
+            <div class="${note.title && 'note-title'}">${note.title}</div>
+            <div class="note-text">${note.text}</div>
+            <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="https://icon.now.sh/palette">
+              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+            </div>
+          </div>
+        </div>
+        `);
     }
   }
   
